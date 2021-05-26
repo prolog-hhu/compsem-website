2dd09bd61ce3673f8b6085c801c4abbb08f90338
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2021-05-26 19:30:34 +0200

    26.05. -- Meeting Changes

---
+++ page-quizzes.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Template Name: Quiz Übersicht
+ * The template for displaying task overview page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#front-page-display
+ *
+ * @package WordPress
+ * @since 2.0.0
+ */
+
+?>
+
+<?php get_header(); ?>
+
+<main class="container-lg px-3 py-6">
+
+    <div class="d-flex flex-wrap flex-justify-center flex-items-center text-center">
+        <article class="col-12 col-md-9 mt-6">
+
+            <?php // get backend content
+            if (have_posts()) {
+
+                // get title
+                the_title('<h1 class="h0-mktg">', '</h1>');
+
+                // Load posts loop.
+                while (have_posts()) {
+                    the_post();
+                
+                    the_content();
+                }
+            } ?>
+
+        </article>
+    </div>
+
+    <div class="gutter d-flex flex-wrap flex-justify-between">
+
+        <?php
+
+            $chapters = new WP_Query(
+                array(
+                    'post_type' =>          'kapitel',
+                    'order' =>              'ASC',
+                    'orderby' =>            'name',
+                    'posts_per_page'=>      -1
+                )
+            );
+
+            if ($chapters->have_posts()) {
+                while ($chapters->have_posts()) {
+                    $chapters->the_post();
+
+                    # skip chapter 0
+                    if(get_the_ID() == 149) {
+                        continue;
+                    }
+
+                    $tasks = new WP_Query(
+                        array(
+                            'post_type'         => 'quizzes',
+                            'order'             => 'ASC',
+                            'orderby'           => 'name',
+                            'posts_per_page'    => -1,
+                            'meta_key'		    => 'kapitel',
+                            'meta_value'	    => get_the_ID(),
+                        )
+                    ); ?>
+
+                    <div class="col-12 my-4">
+
+                        <a  href="<?php the_permalink(); ?>" 
+                            title="<?php the_title_attribute(); ?>"
+                        >
+                            <h2 class="h1-mktg text-gray-dark border-bottom">
+                                <?php the_title(); ?>
+                            </h2>
+                        </a>
+
+                    </div>
+
+        <?php
+                    if ($tasks->have_posts()) {
+                        while ($tasks->have_posts()) {
+                            $tasks->the_post(); ?>
+
+                            <div class="col-12 col-md-6 mb-2">
+                            
+                                <?php get_template_part('template-parts/aufgaben/aufgaben', 'linkbox'); ?>
+
+                            </div>
+
+            <?php
+                        }
+                    } else { ?>
+
+                        <div class="col-12">
+
+                            <div class="flash flash-warn">
+                                <?php _e('Es sind noch keine Aufgaben diesem Kapitel zugeordnet.', 'prolog'); ?>
+                            </div>
+
+                        </div>
+
+            <?php
+                    }
+                    wp_reset_postdata(); ?>
+
+            <?php
+                }
+            }
+                wp_reset_postdata();
+        ?>
+
+    </div>
+
+</main>
+
+<?php get_footer(); ?>
--- template-parts/topbar/topbar-branding.php
+++ template-parts/topbar/topbar-branding.php
@@ -13,14 +13,49 @@
  <a class="d-flex flex-justify-between flex-items-center" href="<?php echo get_bloginfo('url'); ?>"
      title="<?php echo get_bloginfo('description'); ?>">
 
-     <svg class="mr-2 lh-0" width="48" height="48" viewBox="0 0 48 48" aria-hidden="true" version="1.1">
+     <!-- <svg class="mr-2 lh-0" width="48" height="48" viewBox="0 0 48 48" aria-hidden="true" version="1.1">
         <g transform="matrix(0.191001,0,0,0.191001,6.68072,6.03045)">
             <path d="M47.243,12.929L47.243,38.837L68.579,38.837L77.342,65.888L12.192,166.851L44.195,166.851L90.677,98.272L121.156,175.233L169.161,159.993L161.541,136.371L139.444,143.229L87.629,12.929L47.243,12.929Z" style="fill:#ff6702;"/>
         </g>
-     </svg>
+     </svg> -->
+
+     <img 
+        id="branding"
+        class="mr-2 lh-0" 
+        width="48" 
+        height="48"
+        src="<?php echo get_stylesheet_directory_uri(); ?>/media/branding.png"
+     >
 
      <span class="f4 text-bold hide-sm" style="color:#ff6702;">
          <?php echo get_bloginfo('name'); ?>
      </span>
+
+
+     <script>
+        let orginal = "<?php echo get_stylesheet_directory_uri(); ?>/media/branding.png";
+        
+        let cat_meme = "https://i1.sndcdn.com/artworks-RJG4jIrv6lZwWkQH-cWi6nA-t500x500.jpg";
+        let kermit_meme = "https://kermitmemes.com/assets/img/meme_templates/kermit-mouth-shut.jpg";
+
+        document.addEventListener('keydown', function (event) {
+
+            let branding = document.getElementById("branding");
+
+            if (event.key === 'c') {
+                branding.src = cat_meme;
+            };
+
+            if (event.key === 'k') {
+                branding.src = kermit_meme;
+            };
+
+            if (event.key === 'z') {
+                branding.src = orginal;
+            };
+
+        });
+
+     </script>
 
  </a>
